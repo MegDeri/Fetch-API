5d67046698910dbc612f151ce88e03b55cc8d963
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2019-05-01 16:07:39 +0300

    Modify script.js

--- js/script.js
+++ js/script.js
@@ -10,9 +10,9 @@ function getQuote() {
 }
 
 function createTweet(input) {
-  var dataElement = document.querySelector('.box');
+  
   if(input.length === 0) {
-    dataElement.innerHTML = "Something went wrong...we are not able to display a quote." 
+    document.querySelector('.box').innerText = "Something went wrong...we are not able to display a quote." 
     return;
   }
   var data = input[0];
